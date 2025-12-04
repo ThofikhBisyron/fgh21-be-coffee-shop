a22206925d72244474c24e5956c03ce286284098
ThofikhBisyron <sanada.ryuzaki1@gmail.com>
2025-12-04 09:39:50 +0700

    fix config db

--- lib/db.go
+++ lib/db.go
@@ -3,16 +3,22 @@ package lib
 import (
 	"context"
 	"fmt"
+	"os"
 
 	"github.com/jackc/pgx/v5"
 )
 
 func DB() *pgx.Conn {
-	cfg := LoadConfig()
-	conn, err := pgx.Connect(context.Background(), "postgresql://"+cfg.DBUser+":"+cfg.DBPassword+"@"+cfg.DBHost+":"+cfg.DBPort+"/"+cfg.DBName+"?sslmode=disable")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbUser := os.Getenv("DB_USER")
+	dbPass := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
 
+	connStr := fmt.Sprintf("postgresql://%s:%s@%s:%s/%s?sslmode=disable", dbUser, dbPass, dbHost, dbPort, dbName)
+	conn, err := pgx.Connect(context.Background(), connStr)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("DB connection error:", err)
 	}
 	return conn
 }
